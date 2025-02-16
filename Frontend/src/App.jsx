648c8a2a340765d0f5e04aa4c41ab9399727a7bf
use <sathvaraneel52@gmail.com>
2025-02-16 22:40:28 +0530

    adding routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,21 @@
-
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import {Home} from './components/Home'
+import { About } from './components/About'
+import { SignIn } from './components/Signin'
+import { SignUp } from './components/SignUp'
+import {Profile} from './components/Profile'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/sign-in" element={<SignIn/>}/>
+          <Route path="/sign-up" element={<SignUp/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
