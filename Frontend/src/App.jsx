42d868cb64f7cb3beb064072cbd538a99110b577
use <sathvaraneel52@gmail.com>
2025-02-17 17:06:41 +0530

    complete sign in page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import {BrowserRouter , Routes , Route} from 'react-router-dom'
 import {Home} from './Pages/Home'
 import { About } from './Pages/About'
-import { SignIn } from './Pages/Signin'
 import { SignUp } from './Pages/SignUp'
+import { SignIn } from './Pages/SignIn'
 import {Profile} from './Pages/Profile'
 import { Header } from './components/Header'
 
--- Frontend/src/Pages/SignUp.jsx
+++ Frontend/src/Pages/SignUp.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link , useNavigate } from 'react-router-dom';
 
 export function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -33,6 +34,7 @@ export function SignUp() {
       setLoading(false);
       setError(true);
     }
+    navigate('/sign-in');
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
